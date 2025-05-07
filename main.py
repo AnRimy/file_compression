aa8957b0bec9ec085bd2cdbe7a4cccb97539022e
AnRimy <ardk3@bk.ru>
2025-05-07 22:03:06 +0000

    error fix

--- main.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from mainWindow import MainWindow
-
-if __name__ == '__main__':
-    app = QApplication([])
-    window = MainWindow()
-    window.run()
-    app.exec()
